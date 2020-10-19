c3cb09a8e31e68f9d4abe7b4e6b75509f3f2236a
t0rb3n <torben@herborn.me>
2020-10-19 10:09:16 +0200

    test for postgres on heroku

--- dbexport.js
+++ dbexport.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-
+/*
 const sequelize = new Sequelize('database', 'user', 'password', {
 	host: 'localhost',
 	dialect: 'sqlite',
@@ -8,6 +8,23 @@ const sequelize = new Sequelize('database', 'user', 'password', {
 	// SQLite only
 	storage: './database.sqlite',
 });
+	new Sequelize('postgres://:@:5432/');
+
+*/
+
+const sequelize = new Sequelize('ddie8i3m9aiv64', 'ddie8i3m9aiv64', 'a21c55fdc86698481dafc386e2035d9fa3824a6ceb736839c879ee91dfcee79b', {
+	host: 'ec2-34-251-118-151.eu-west-1.compute.amazonaws.com',
+	dialect: 'postgres',
+	port: 5432,
+	logging: false,
+	dialectOptions: {
+		ssl: true,
+	},
+});
+
+
+checkConnection(sequelize);
+
 
 const Tags = sequelize.define('tags', {
 	serverid: {
@@ -27,7 +44,6 @@ const Tags = sequelize.define('tags', {
 });
 
 
-
 Tags.sync()
 	.then(() => {
 		console.log('Database & tables created!');
@@ -35,3 +51,14 @@ Tags.sync()
 
 // Tags.on('error', err => console.log('Connection Error', err));
 module.exports = Tags;
+
+
+async function checkConnection(seq) {
+	try {
+		await seq.authenticate();
+		console.log('Connection has been established successfully.');
+	}
+	catch (error) {
+		console.error('Unable to connect to the database:', error);
+	}
+}
